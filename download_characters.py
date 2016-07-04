365caee96eb34e999437779b2426afde9d6ddb79
Mark Needham <mark.needham@neotechnology.com>
2016-07-04 07:02:02 +0100

    moar characters

--- download_characters.py
+++ download_characters.py
@@ -2,14 +2,18 @@ import bs4
 import csv
 import requests
 
-with open("data/import/characters.csv", "r") as episodes_file:
+import os
+
+with open("data/characters_to_download.csv", "r") as episodes_file:
     reader = csv.reader(episodes_file, delimiter = ",")
     next(reader, None)
 
     for row in reader:
         name = row[0].split("/")[-1]
-        print name, row[0]
+        download_location = "data/wikia/characters/{0}".format(name)
 
-        page = requests.get("http://gameofthrones.wikia.com{0}".format(row[0]))
-        with open("data/wikia/characters/{0}".format(name), 'wb') as test:
-            test.write(page.content)
+        if not os.path.isfile(download_location):
+            print name, row[0]
+            page = requests.get("http://gameofthrones.wikia.com{0}".format(row[0]))
+            with open(download_location, 'wb') as test:
+                test.write(page.content)
